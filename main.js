a4de7a4bbc4737d6dc1f73c0d0fbafff326ff8c9
sqfasd <sqf1225@foxmail.com>
2016-05-12 06:32:09 +0800

    add more log info

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ var nodes = [];
 function main() {
   async.series([
     function(next) {
-      console.log('step 1 ...');
+      console.log('step 1 init nodes ...');
       for (var i = 0; i < 20; i++) {
         nodes[i] = new Node(i);
       }
       setTimeout(next, 1000);
     },
     function(next) {
-      console.log('step 2 ...');
+      console.log('step 2 init p2p network ...');
       for (var i in nodes) {
         nodes[i].connect();
       }
       setTimeout(next, 2000);
     },
     function(next) {
-      console.log('step 3');
+      console.log('step 3 start forging');
       for (var i in nodes) {
         nodes[i].start();
       }
